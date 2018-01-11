41398b417f419367a21c282fa6801f93d1c0482c
MacRayy <mecrai@gmail.com>
2018-01-11 14:34:32 +0100

    feat(app): add basic data binding

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ new Vue ({
 
 	data: {
 		name: 'Berta',
-		job: 'Dog'
+		job: 'Dog',
+		insta: 'https://www.instagram.com/explore/tags/foxiberta/',
+		instaTag: '<a href="https://www.instagram.com/explore/tags/foxiberta/">FoxiBerta insta</a>'
 	},
 
 	methods: {
