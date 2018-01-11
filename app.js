42f9792e79fbdb49f4d6cd6702d6633039dfbea4
MacRayy <mecrai@gmail.com>
2018-01-11 14:54:56 +0100

    feat(app): add basic event handling

--- app.js
+++ app.js
@@ -5,12 +5,28 @@ new Vue ({
 		name: 'Berta',
 		job: 'Dog',
 		insta: 'https://www.instagram.com/explore/tags/foxiberta/',
-		instaTag: '<a href="https://www.instagram.com/explore/tags/foxiberta/">FoxiBerta insta</a>'
+		instaTag: '<a href="https://www.instagram.com/explore/tags/foxiberta/">FoxiBerta insta</a>',
+		age: 1,
+		x: 0,
+		y: 0
 	},
 
 	methods: {
 		greet: function(time) {
 			return 'woof ' + time + ' ' + this.name
+		},
+
+		add: function(dec) {
+			this.age += dec
+		},
+
+		subtract: function(inc) {
+			this.age -= inc
+		},
+
+		updateXY: function(event) {
+			this.x = event.offsetX
+			this.y = event.offsetY
 		}
 	}
 })
