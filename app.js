b9ba0efc7d8c8c82eadafe161f65f95d756624d2
MacRayy <mecrai@gmail.com>
2018-01-12 18:16:31 +0100

    feat(punch game): create simple and boring punch game

--- app.js
+++ app.js
@@ -2,71 +2,21 @@ new Vue ({
 	el: '#vue-app',
 
 	data: {
-		name: 'Berta',
-		job: 'Dog',
-		insta: 'https://www.instagram.com/explore/tags/foxiberta/',
-		instaTag: '<a href="https://www.instagram.com/explore/tags/foxiberta/">FoxiBerta insta</a>',
-		age: 1,
-		x: 0,
-		y: 0,
-		inputName: '',
-		a: 0,
-		b: 0,
-		available: false,
-		nearby: false,
-		error: false,
-		success: false,
-		characters: ['mario', 'luigi', 'wario', 'link'],
-		jedis: [
-			{name: 'obi-wan', color: 'blue'},
-			{name: 'luke skywlaker', color: 'green'},
-			{name: 'yoda', color: 'green'}
-		]
+		health: 100,
+		ended: false
 	},
 
 	methods: {
-		greet: function(time) {
-			return 'woof ' + time + ' ' + this.name
-		},
-
-		add: function(dec) {
-			this.age += dec
-		},
-
-		subtract: function(inc) {
-			this.age -= inc
-		},
-
-		updateXY: function(event) {
-			this.x = event.offsetX
-			this.y = event.offsetY
-		},
-
-		preventer: function() {
-			alert('visit prevented')
-		},
-
-		logAlma: function() {
-			console.log('alma')
-		}
-	},
-
-	computed: {
-		addToA: function() {
-			console.log('auto')
-			return this.age + this.a
-		},
-
-		addToB: function() {
-			console.log('banan')
-			return this.age + this.b
-		},
-
-		compClasses: function() {
-			return {
-				available: this.available,
-				nearby: this.nearby
+		punch: function() {
+			this.health -= 10
+			if (this.health <= 0) {
+				this.ended = true
 			}
+		},
+
+		restart: function() {
+			this.health = 100
+			this.ended = false
 		}
 	}
 })
