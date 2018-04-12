22d368111a50c7c00211f788b63368ced86f4bd8
Zsolt Makrai <zsolt.makrai@blackswan.com>
2018-04-12 17:04:07 +0200

    feat(app): create basic vue app with cli

---
+++ vue-playlist/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>{{title}}</h1>
+    <p>{{greeting()}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'first vue file'
+    }
+  },
+
+  methods: {
+    greeting() {
+      return 'hello there!'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+</style>
