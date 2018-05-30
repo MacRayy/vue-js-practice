ecd94223c8a54a2433acd234e50ba8f8f97921ed
Zsolt Makrai <zsolt.makrai@blackswan.com>
2018-05-30 12:16:31 +0200

    feat(form helper): create form helper and use it as slot

--- vue-playlist/src/App.vue
+++ vue-playlist/src/App.vue
@@ -2,6 +2,21 @@
 	<div>
 		<app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
 		<app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
+
+		<form-helper>
+			<div slot="form-header">
+				<h3>Form title from slot</h3>
+				<p>information</p>
+			</div>
+			<div slot="form-fields">
+				<input type="text" placeholder="name">
+				<input type="password" placeholder="password">
+			</div>
+			<div slot="form-controls">
+				<button v-on:click="handleSubmit">send</button>
+			</div>
+		</form-helper>
+
 		<app-footer v-bind:title="title"></app-footer>
 	</div>
 </template>
@@ -10,12 +25,14 @@
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
 import Ninjas from './components/Ninjas.vue'
+import formHelper from './components/FormHelper.vue'
 
 export default {
 	components: {
 		'app-header': Header,
 		'app-footer': Footer,
-		'app-ninjas': Ninjas
+		'app-ninjas': Ninjas,
+		'form-helper': formHelper
 	},
 
 	data () {
@@ -35,6 +52,11 @@ export default {
 	methods: {
 		updateTitle(updatedTitle) {
 			this.title = updatedTitle
+		},
+
+		handleSubmit(event) {
+			event.preventDefault()
+			console.log('send!!!!!!!')
 		}
 	}
 }
---
+++ vue-playlist/src/components/FormHelper.vue
@@ -0,0 +1,36 @@
+<template>
+	<footer>
+		<h1>Please fill out the form</h1>
+		<form>
+			<div id="form-header">
+				<slot name="form-header"></slot>
+			</div>
+			<div id="form-fields">
+				<slot name="form-fields"></slot>
+			</div>
+			<div id="form-controls">
+				<slot name="form-controls"></slot>
+			</div>
+			<div id="useful-links">
+				<ul>
+					<li><a href="#">Link 1</a></li>
+					<li><a href="#">Link 2</a></li>
+					<li><a href="#">Link 3</a></li>
+				</ul>
+			</div>
+		</form>
+	</footer>
+</template>
+
+<script>
+
+export default {
+	data () {
+
+	}
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
