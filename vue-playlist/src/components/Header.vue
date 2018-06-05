28f74318954773a4ad837ee2985adc3e7da1691b
Zsolt Makrai <zsolt.makrai@blackswan.com>
2018-06-05 11:23:12 +0200

    feat(nav): add style tp active links

--- vue-playlist/src/components/Header.vue
+++ vue-playlist/src/components/Header.vue
@@ -1,6 +1,12 @@
 <template>
 	<header>
 		<h1 v-on:click="changeTitle">{{title}}</h1>
+		<nav>
+			<ul>
+				<li><router-link to="/" exact>Blogs</router-link></li>
+				<li><router-link to="/add" exact>Add blog</router-link></li>
+			</ul>
+		</nav>
 	</header>
 </template>
 
@@ -40,5 +46,33 @@ header {
 h1 {
 	color: #222;
 	text-align: center;
+}
+
+ul {
+  list-style-type: none;
+  text-align: center;
+  margin: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+a {
+  color: #fff;
+  text-decoration: none;
+  padding: 6px 8px;
+  border-radius: 10px;
+}
+
+nav {
+  background: #444;
+  padding: 14px 0;
+  margin-bottom: 40px;
+}
+
+.router-link-active {
+  background: #eee;
+  color: #444;
 }
 </style>
